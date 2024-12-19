85ca4f83369732b83a39a5194754b11d07dc48a8
Arch-AMichael <139619784+Arch-AMichael@users.noreply.github.com>
2024-12-19 15:33:10 +0000

    Thu, Dec 19, 2024, 10:33 AM -05:00

--- indoor/indoor.py
+++ indoor/indoor.py
@@ -1,7 +1,2 @@
-Name = (input("Hi and welcome to Mcdonald, what would you like to get today? ")).lower().strip()
-
-print(f"Sure, 1 {Name} coming up")
-
-Second_option = (input("Anything else? ")).lower().strip()
-
-print(f"Awesome, thank you for coming to Mcdonld, Your order of {Name} and {Second_option} will be ready in 3 minutes")
+msg = input("Message")
+print (msg. lower())
