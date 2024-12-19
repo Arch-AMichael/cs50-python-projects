0e8e36c79b59c026fb244210c1d6565cbf946547
Arch-AMichael <139619784+Arch-AMichael@users.noreply.github.com>
2024-12-19 16:27:39 +0000

    Thu, Dec 19, 2024, 11:27 AM -05:00

--- einstein/einstein.py
+++ einstein/einstein.py
@@ -1,6 +1,12 @@
     #Get user input
 Mass = int(input("m: "))
     #Convert Mass to Energy
-Energy = Mass * 90000000000000000
+
+Energy = (Mass) * 90000000000000000
     #Print Mass using J
 print(f"E: {Energy}")
+
+
+#You can add int either on when recieveing the input or when doing the caculation
+#Input: int(input("m: ")))
+#Caculation: int(Mass) * 90000000000000000
