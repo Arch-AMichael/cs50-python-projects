0ca97776c045466c669a9e24775581bc84d2a2f8
Arch-AMichael <139619784+Arch-AMichael@users.noreply.github.com>
2024-12-21 23:16:15 +0000

    Sat, Dec 21, 2024, 6:16 PM -05:00

---
+++ extensions/extensions.py
@@ -0,0 +1,21 @@
+def main():
+    answer = input("File name: ")
+    extension = check(answer)
+    print(f"{extension})
+
+
+def check(x):
+    if "gif" in x
+        print (f"image/gif")
+    elif "gif" in x
+        print (f"image/gif")
+    elif "gif" in x
+        print (f"image/gif")
+    elif "gif" in x
+        print (f"image/gif")
+    elif "gif" in x
+        print (f"image/gif")
+        else "gif" in x
+        print (f"image/gif")
+
+
