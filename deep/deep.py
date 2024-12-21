4a1e94aa58b8a21f868c77d95a287c8b24675fa3
Arch-AMichael <139619784+Arch-AMichael@users.noreply.github.com>
2024-12-21 06:58:35 +0000

    Sat, Dec 21, 2024, 1:58 AM -05:00

--- deep/deep.py
+++ deep/deep.py
@@ -2,10 +2,11 @@
 def main():
     Q_of_life = input("What is the great question of life ").strip().lower()
     Q_of_life = Q_of_life.replace("-","").replace(" ","")
-    
-    if Q_of_life == "42" or Q_of_life == "forty two":
+
+    if Q_of_life == "42" or Q_of_life == "fortytwo":
         print("Yes")
     else:
         print ("No")
 
 main()
+
