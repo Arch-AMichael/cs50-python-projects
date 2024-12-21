ba0885323af3b810b588e4a480f830f3bdc9408e
Arch-AMichael <139619784+Arch-AMichael@users.noreply.github.com>
2024-12-21 07:05:29 +0000

    Sat, Dec 21, 2024, 2:05 AM -05:00

--- deep/deep.py
+++ deep/deep.py
@@ -10,4 +10,12 @@ def main():
 
 main()
 
-#
+# Smarter way to solve the problem is to replace all the expected inputs to one input you desire and using that.
+# For example, instead of
+#   if Q_of_life == "42" or Q_of_life == "fortytwo":
+#        print("Yes")
+
+#use
+# Q_of_life = Q_of_life.replace("-","")
+
+# that way any forty-two will become fortytwo. This is applicable to spaces as well
