e076fbb00f8757425c548c2dd636cf6791cb06bd
Arch-AMichael <139619784+Arch-AMichael@users.noreply.github.com>
2024-12-21 23:20:39 +0000

    Sat, Dec 21, 2024, 6:20 PM -05:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -5,17 +5,15 @@ def main():
 
 
 def check(x):
-    if "gif" in x
-        print (f"image/gif"):
-    elif "jpeg" in x or ".jpg" in x
+    if ".gif" in x:
+        print (f"image/gif")
+    elif ".jpeg" in x or ".jpg" in x:
         print (f"image/jpeg")
-    elif "gif" in x
-        print (f"image/gif")
-    elif "gif" in x
-        print (f"image/gif")
-    elif "gif" in x
-        print (f"image/gif")
-        else "gif" in x
+    elif ".png" in x:
+        print (f"image/png")
+    elif ".txt" in x:
+        print (f"text/plain)
+    else "gif" in x:
         print (f"image/gif")
 
 
