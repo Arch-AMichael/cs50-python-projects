14079046653172654c10a16f9a9ac23cc0dd8a87
Arch-AMichael <139619784+Arch-AMichael@users.noreply.github.com>
2024-12-31 17:40:13 +0000

    Tue, Dec 31, 2025, 12:40 PM -05:00

--- meal/meal.py
+++ meal/meal.py
@@ -5,8 +5,7 @@ def main():
 
 
 def convert(hours, minutes):
-    caculation = hours + minutes / 60
-    
+    caculation = minutes / 60
     if hours = "7" or "8":
         return ("Breakfast Time")
     if hours = "12" or "13":
