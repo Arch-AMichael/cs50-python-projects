dafd2b55a45f2545fa90b2313b565a3a57483ad6
Arch-AMichael <139619784+Arch-AMichael@users.noreply.github.com>
2024-12-31 17:42:10 +0000

    Tue, Dec 31, 2025, 12:42 PM -05:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,11 +1,12 @@
 def main():
     time = input("What is the time?")
     hours, minutes = time.split(":")
-    newtime = convert(float(hours), float(minutes))
+    newtime = convert(hours), (float(minutes))
 
 
 def convert(hours, minutes):
     caculation = minutes / 60
+    
     if hours = "7" or "8":
         return ("Breakfast Time")
     if hours = "12" or "13":
