ca21a9951105c204259c23888c888c2e46b663c0
Arch-AMichael <139619784+Arch-AMichael@users.noreply.github.com>
2024-12-31 18:32:51 +0000

    Tue, Dec 31, 2025, 1:32 PM -05:00

--- meal/meal.py
+++ meal/meal.py
@@ -3,11 +3,11 @@ def main():
     newtime = convert(time)
 
 
-    if newtime == 7 or newtime == 8:
+    if newtime <= 7 or newtime <= 8:
         print("Breakfast Time")
-    elif newtime == 12 or newtime == 13:
+    elif newtime <= 12 or newtime <= 13:
         print("Lunch Time")
-    elif newtime == 18 or newtime == 19:
+    elif newtime <= 18 or newtime <= 19:
         print("Dinner Time")
 
 def convert(time):
