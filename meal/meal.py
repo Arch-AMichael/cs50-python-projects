4947a5d0d7a226bb4c1d2e2f1ec4d5f70d1e976b
Arch-AMichael <139619784+Arch-AMichael@users.noreply.github.com>
2024-12-31 17:48:54 +0000

    Tue, Dec 31, 2025, 12:48 PM -05:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,15 +1,16 @@
 def main():
     time = input("What is the time?")
-    hours, minutes = time.split(":")
-    newtime = convert(hours), (float(minutes))
+    time_parts = time.split(":")
+    result = convert(time_parts[0])
+    print (f"{result}")
 
 
-def convert(hours):
-    if hours = "7" or "8":
+def convert(hour):
+    if hour = "7" or "8":
         return ("Breakfast Time")
-    if hours = "12" or "13":
+    if hour = "12" or "13":
         return ("Lunch Time")
-    if hours = "7" or "19":
+    if hour = "7" or "19":
         return ("Breakfast Time")
 
 
