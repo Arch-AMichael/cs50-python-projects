26f961e2b85fbf9c8ca2e69266ee19837c47d30e
Arch-AMichael <139619784+Arch-AMichael@users.noreply.github.com>
2025-05-05 17:27:01 +0000

    Mon, May 5, 2025, 1:27 PM -04:00

---
+++ test_bank/bank.py
@@ -0,0 +1,30 @@
+#In a file called bank.py,
+# implement a program that prompts the user for a greeting.
+# If the greeting starts with “hello”, output $0.
+# If the greeting starts with an “h” (but not “hello”), output $20.
+# Otherwise, output $100.
+# Ignore any leading whitespace in the user’s greeting,
+# and treat the user’s greeting case-insensitively
+
+def main():
+    greeting = input("Greeting:  ").strip().lower()
+
+    if "hello" in greeting:
+        print(f"$0")
+
+    elif greeting.startswith("h"):
+        print(f"$20")
+
+    else:
+        print(f"$100")
+
+
+if __name__ == "__main__":
+    main()
+
+
+# New method startswith()
+# In Python, you can use the startswith() method to check if a string starts with a specific prefix.
+
+#OR use Indexing
+#elif "h" in greeting [0]
