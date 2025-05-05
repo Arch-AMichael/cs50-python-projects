14f7d790749ad84d3d9dea920f38edccc035785b
Arch-AMichael <139619784+Arch-AMichael@users.noreply.github.com>
2025-05-05 17:39:18 +0000

    Mon, May 5, 2025, 1:39 PM -04:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -7,6 +7,6 @@ def test_mistake():
     assert value('Hey you') == '$0'
 
 def test_correct():
-    assert value('Hello') == '$100'
+    assert value('Hello') == '$0'
     assert value('Whats up') == '$100'
-    assert value('Hey you') == '$100'
+    assert value('Hey you') == '$20'
